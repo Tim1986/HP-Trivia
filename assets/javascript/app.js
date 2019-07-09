261de3fd68018f5ad66dba2e563360224fad3940
Timothy Brahm <timothy.david.brahm@gmail.com>
2019-07-09 10:03:46 -0400

    made code much less wet

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,56 +7,14 @@ var intervalId
 function startTimer() {
     intervalId = setInterval(decrement, 1000);
 }
-
-var decrement = function() {
-    timeLeft--;
-    $("#time-left").text(timeLeft);
-    if (timeLeft === 0) {
-        clearInterval(intervalId);   
-        tally();
-        $(".hide").css('display', 'none');
-        $(".hide2").css('display', 'unset');
-        $("#wins").text(correct);
-        $("#losses").text(incorrect);
-        $("#ties").text(unanswered); 
-        if (correct === 8) {
-            $(".some-incorrect").css('display', 'none')
-            $(".all-correct").css('display', 'unset')
-            $("#grade").text("A (Outstanding)")
-        } else if (correct === 7) {
-            $("#grade").text("A- (Exceeds Expectations)")
-        } else if (correct === 6) {
-            $("#grade").text("B+ (Acceptable)")
-        } else if (correct === 5) {
-            $("#grade").text("B- (Acceptable)")
-        } else if (correct === 4) {
-            $("#grade").text("C+ (Poor)")
-        } else if (correct === 3) {
-            $("#grade").text("C- (Poor)")
-        } else if (correct === 2) {
-            $("#grade").text("D+ (Dreadful)")
-        } else if (correct === 1) {
-            $("#grade").text("D- (Dreadful)")
-        } else {
-            $("#grade").text("F (Troll), You Are Expelled From Hogwarts")
-        }     
-    }
-}
-
-$(".start").on("click", function () {
-    $(".hide1").css('display', 'none');
-    $(".hide").css('display', 'unset');
-    startTimer();
-})
-
-$(".done").on("click", function () {
-    clearInterval(intervalId)
-    tally()
+var endQuiz = function() {
+    clearInterval(intervalId);   
+    tally();
     $(".hide").css('display', 'none');
     $(".hide2").css('display', 'unset');
     $("#wins").text(correct);
     $("#losses").text(incorrect);
-    $("#ties").text(unanswered);
+    $("#ties").text(unanswered); 
     if (correct === 8) {
         $(".some-incorrect").css('display', 'none')
         $(".all-correct").css('display', 'unset')
@@ -77,89 +35,59 @@ $(".done").on("click", function () {
         $("#grade").text("D- (Dreadful)")
     } else {
         $("#grade").text("F (Troll), You Are Expelled From Hogwarts")
+    }     
+}
+
+var decrement = function() {
+    timeLeft--;
+    $("#time-left").text(timeLeft);
+    if (timeLeft === 0) {
+        endQuiz()
     }
+}
+
+$(".start").on("click", function () {
+    $(".hide1").css('display', 'none');
+    $(".hide").css('display', 'unset');
+    startTimer();
 })
 
+$(".done").on("click", function () {
+    endQuiz()
+})
+
+// The fact that this is an array of objects is doing nothing for me. It might as well be an array of answers.
+var answers = [
+    {q: "Flamel Age", a: "665"},
+    {q: "Joke Shop", a: "Weasley's Wizard Wheezes"},
+    {q: "OWL", a: "Ordinary Wizarding Level"},
+    {q: "Grangers", a: "Dentists"},
+    {q: "Sorting Hat", a: "Godric Gryffindor"},
+    {q: "First Kiss", a: "Cho Chang"},
+    {q: "Vernon", a: "Grunnings"},
+    {q: "Veela", a: "Fleur Delacour"},
+]
+
+var questions = []
+
 var tally = function() {
-    var question1 = document.quiz.question1.value;
-    var question2 = document.quiz.question2.value;
-    var question3 = document.quiz.question3.value;
-    var question4 = document.quiz.question4.value;
-    var question5 = document.quiz.question5.value;
-    var question6 = document.quiz.question6.value;
-    var question7 = document.quiz.question7.value;
-    var question8 = document.quiz.question8.value;
+    questions.push(document.quiz.question0.value)
+    questions.push(document.quiz.question1.value)
+    questions.push(document.quiz.question2.value)
+    questions.push(document.quiz.question3.value)
+    questions.push(document.quiz.question4.value)
+    questions.push(document.quiz.question5.value)
+    questions.push(document.quiz.question6.value)
+    questions.push(document.quiz.question7.value)
 
-if (question1 === "665") {
-    correct++;
-    $(".q1a").css('display', 'none');
-} else if (question1 === "") {
-    unanswered++;
-} else {
-    incorrect++;
-}
-
-if (question2 === "Weasley's Wizard Wheezes") {
-    correct++;
-    $(".q2a").css('display', 'none');
-} else if (question2 === "") {
-    unanswered++;
-} else {
-    incorrect++;
-}
-
-if (question3 === "Ordinary Wizarding Level") {
-    correct++;
-    $(".q3a").css('display', 'none');
-} else if (question3 === "") {
-    unanswered++;
-} else {
-    incorrect++;
-}
-
-if (question4 === "Dentists") {
-    correct++;
-    $(".q4a").css('display', 'none');
-} else if (question4 === "") {
-    unanswered++;
-} else {
-    incorrect++;
-}
-
-if (question5 === "Godric Gryffindor") {
-    correct++;
-    $(".q5a").css('display', 'none');
-} else if (question5 === "") {
-    unanswered++;
-} else {
-    incorrect++;
-}
-
-if (question6 === "Cho Chang") {
-    correct++;
-    $(".q6a").css('display', 'none');
-} else if (question6 === "") {
-    unanswered++;
-} else {
-    incorrect++;
-}
-
-if (question7 === "Grunnings") {
-    correct++;
-    $(".q7a").css('display', 'none');
-} else if (question7 === "") {
-    unanswered++;
-} else {
-    incorrect++;
-}
-
-if (question8 === "Fleur Delacour") {
-    correct++;
-    $(".q8a").css('display', 'none');
-} else if (question8 === "") {
-    unanswered++;
-} else {
-    incorrect++;
-}
-
+    for (var i = 0; i < answers.length; i++) {
+        if (answers[i].a === questions[i]) {
+            correct++;
+            $(".q" + i + "a").css('display', 'none');
+        } else if (questions[i] === "") {
+            unanswered++;
+        } else {
+            incorrect++;
+        }
+    }
 }
